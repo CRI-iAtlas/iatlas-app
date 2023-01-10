4fcca732e96435f79cd42bb874cd5466c34ff49f
heimannch <carolina.heimann@gmail.com>
2023-01-09 16:33:32 -0800

    datasets checkbox with native Shiny. TOC as a function

--- R/new_dataset_selection_server.R
+++ R/new_dataset_selection_server.R
@@ -1,16 +1,40 @@
 new_dataset_selection_server <- function(id, cohort_obj){
+
   shiny::moduleServer(
     id,
     function(input, output, session) {
-print("hey")
-      # sc_bubbleplot_server(
-      #   "sc_bubbleplot",
-      #   cohort_obj
-      # )
+        ns <- session$ns
+        datasets <- list('Cancer Genomics' = c('TCGA', 'PCAWG'),
+                         'Immunotherapy' = c('Choueiri 2016 - KIRC, PD-1', 'Gide 2019 - SKCM, PD-1 +/- CTLA4'))
 
+
+
+        output$datasets_toc <- shiny::renderUI({
+            tags$div(
+              id = 'toc_container',
+              tags$p(
+                class = 'toc_title',
+                'Contents'
+              ),
+              tags$ul(
+                class = 'toc_list',
+                #toc
+                tags$li(tags$a(href = '#Cancer Genomics', 'Cancer Genomics')), tags$li(tags$a(href = '#Immunotherapy', 'Immunotherapy'))
+                #lazyeval::lazy_eval(paste_dataset_level_titles(datasets))
+                    )
+            )
+
+
+        })
+
+        output$interactionUI <- shiny::renderUI({
+
+          DT::datatable(ioresponse_data$dataset_df %>%
+                           select(Dataset, Study, Antibody, Samples, Patients, `Sequencing Method`))
+
+          # all_boxes <- get_listed_boxes_with_headers(datasets, session)
+          # do.call(fluidRow, all_boxes)
+        })
     }
   )
 }
-
-
-
--- R/new_dataset_selection_ui.R
+++ R/new_dataset_selection_ui.R
@@ -1,142 +1,12 @@
 new_dataset_selection_ui <- function(id){
 
     ns <- shiny::NS(id)
-
-    datasets <- list('Cancer Genomics' = c('TCGA', 'PCAWG'),
-                     'Immunotherapy' = c('Choueiri 2016 - KIRC, PD-1', 'Gide 2019 - SKCM, PD-1 +/- CTLA4'))
-
-    library(shinyWidgets)
-
-    generateAwesomeOptions2 <- function (inputId, choices, selected, inline, status, flag = FALSE) {
-
-        # if input is a list, flag will be set to `TRUE` by the calling function
-        if (flag) {
-
-            options <-  mapply(choices, names(choices), FUN = function(lchoices, lname) {
-
-                lchoices <- shinyWidgets:::choicesWithNames(lchoices)
-
-                tags$div(
-                    tags$a(lname, href = paste0("#", lname), style = "margin-bottom: 10px;"),
-
-                    mapply(lchoices, names(lchoices), FUN = function(value, name) {
-
-                        inputTag <- tags$input(type = "checkbox", name = inputId,
-                                               value = value, id = paste0(inputId, value))
-                        if (value %in% selected)
-                            inputTag$attribs$checked <- "checked"
-                        if (inline) {
-                            tags$div(class = paste0("awesome-checkbox checkbox-inline checkbox-",
-                                                    status), inputTag, tags$label(name, `for` = paste0(inputId,
-                                                                                                       value)))
-                        }
-                        # flag is not set `TRUE` this will create the normal checkboxes
-                        else {
-                            tags$div(class = paste0("awesome-checkbox checkbox-",
-                                                    status), inputTag, tags$label(name, `for` = paste0(inputId,
-                                                                                                       value)))
-                        }
-                    }, SIMPLIFY = FALSE, USE.NAMES = FALSE)
-                )
-
-            }, SIMPLIFY = FALSE, USE.NAMES = FALSE)} else {
-
-                options <- mapply(choices, names(choices), FUN = function(value,
-                                                                          name) {
-                    inputTag <- tags$input(type = "checkbox", name = inputId,
-                                           value = value, id = paste0(inputId, value))
-                    if (value %in% selected)
-                        inputTag$attribs$checked <- "checked"
-                    if (inline) {
-                        tags$div(class = paste0("awesome-checkbox checkbox-inline checkbox-",
-                                                status), inputTag, tags$label(name, `for` = paste0(inputId,
-                                                                                                   value)))
-                    }
-                    else {
-                        tags$div(class = paste0("awesome-checkbox checkbox-",
-                                                status), inputTag, tags$label(name, `for` = paste0(inputId,
-                                                                                                   value)))
-                    }
-                }, SIMPLIFY = FALSE, USE.NAMES = FALSE)
+#
+#     datasets <- list('Cancer Genomics' = c('TCGA', 'PCAWG'),
+#                      'Immunotherapy' = c('Choueiri 2016 - KIRC, PD-1', 'Gide 2019 - SKCM, PD-1 +/- CTLA4'))
 
 
-            }
-
-        tags$div(class = "shiny-options-group", options)
-
-
-    }
-
-    awesomeCheckboxGroup2 <- function (inputId, label, choices, selected = NULL, inline = FALSE,
-                                       status = "primary", width = NULL) {
-        if(!is.list(choices)) {
-
-            choices <- shinyWidgets:::choicesWithNames(choices)
-            selected <- shiny::restoreInput(id = inputId, default = selected)
-            if (!is.null(selected))
-                selected <- shinyWidgets:::validateSelected(selected, choices, inputId)
-            options <- generateAwesomeOptions2(inputId, choices, selected,
-                                               inline, status = status)
-
-        } else {
-            choices2 <- unlist(unname(choices))
-            choices2 <- shinyWidgets:::choicesWithNames(choices2)
-            selected <- shiny::restoreInput(id = inputId, default = selected)
-
-            if (!is.null(selected))
-                selected <- shinyWidgets:::validateSelected(selected, choices2, inputId)
-            options <- generateAwesomeOptions2(inputId, choices, selected,
-                                               inline, status = status, flag = TRUE)
-        }
-
-        divClass <- "form-group shiny-input-container shiny-input-checkboxgroup awesome-bootstrap-checkbox"
-        if (inline)
-            divClass <- paste(divClass, "shiny-input-container-inline")
-        awesomeTag <- tags$div(id = inputId, style = if (!is.null(width))
-            paste0("width: ", validateCssUnit(width), ";"), class = divClass,
-            tags$label(label, `for` = inputId, style = "margin-bottom: 10px;"),
-            options)
-        shinyWidgets:::attachShinyWidgetsDep(awesomeTag, "awesome")
-    }
-
-    css <- "
-.chapter {
-  color: black;
-  text-align: left;
-  font-family: Georgia;
-  font-size: 20px;
-}
-
-.chapter-container {
-  background-color: white;
-  border-radius: 10px;
-}
-
-#toc_container {
-  position: fixed;
-  top: 0;
-  background: #f9f9f9 none repeat scroll 0 0;
-  border: 1px solid #aaa;
-  display: table;
-  font-size: 95%;
-  margin-bottom: 1em;
-  padding: 20px;
-  width: auto;
-}
-
-.toc_title {
-  font-weight: 700;
-  text-align: center;
-}
-
-#toc_container li,
-#toc_container ul,
-#toc_container ul li {
-  list-style: outside none none !important;
-}
-"
-
-tags$head(tags$style(HTML(css)))
+    tags$head(tags$style(HTML("css/table_of_contents.css")))
 
 
     shiny::tagList(
@@ -151,37 +21,18 @@ tags$head(tags$style(HTML(css)))
             width = 3,
             column(
                 width = 12,
-                tags$div(
-                    id = "toc_container",
-                    tags$p(
-                        class = "toc_title",
-                        "Contents"
-                    ),
-                    tags$ul(
-                        class = "toc_list",
-                        tags$li(
-                            tags$a(
-                                href = "#introduction",
-                                "Introduction"
-                            )
-                        ),
-                        tags$li(
-                            tags$a(
-                                href = "#Immunotherapy",
-                                "Immunotherapy"
-                            )
-                        )
-                    )
-                )
+                shiny::uiOutput(ns("datasets_toc"))
+
             )
         ),
         iatlas.modules::optionsBox(
             width = 9,
-            awesomeCheckboxGroup2(
-                inputId = "somevalue",
-                label = "",
-                choices = datasets
-            )
+            # awesomeCheckboxGroup2(
+            #     inputId = "somevalue",
+            #     label = "",
+            #     choices = datasets
+            # ),
+            shiny::uiOutput(ns("interactionUI"))
         )
     )
 }
